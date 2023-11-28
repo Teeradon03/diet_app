d5f5744e9347dd10ce01373cd3c64fe2d0036da3
teeradon <teeradon.zoo@gmail.com>
2023-11-28 17:02:26 +0700

    remove login404 component

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import Form from './components/form/Form';
 import Home from './components/home/Home';
 import Question from './components/question/Question';
 import NotFound404 from './pages/404/NotFound404';
-import Login_404 from './components/login/Login_404'
 import HomePage from './pages/home/HomePage';
 import Ever from './components/ever/Ever';
 import Never from './components/never/Never';
@@ -20,7 +19,6 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={<NotFound404 />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/login_404" element={<Login_404 />} />
         <Route path="/form" element={<Form />} />
         <Route path="/home" element={<Home />} />
         <Route path="/question" element={<Question />} />
