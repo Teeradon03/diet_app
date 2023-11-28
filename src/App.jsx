7bf9540b64a9d1aa6c455b94b95674d7dc8e04ae
teeradon <teeradon.zoo@gmail.com>
2023-11-28 14:45:15 +0700

    adding login page and function

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
+// import "bootstrap/dist/css/bootstrap.css"
 
 import './App.css'
 import { BrowserRouter, Routes, Route, } from "react-router-dom";
 import Login from './components/login/Login'
 import Form from './components/form/Form';
-import "bootstrap/dist/css/bootstrap.min.css"
-import Register from './components/register/Register';
 import Home from './components/home/Home';
 import Question from './components/question/Question';
-
+import Card from './components/card/Card';
+import NotFound404 from './pages/404/NotFound404';
+import Login_404 from './components/login/Login_404'
+import HomePage from './pages/home/HomePage';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<></>} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<NotFound404 />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/login_404" element={<Login_404 />} />
         <Route path="/form" element={<Form />} />
         <Route path="/home" element={<Home />} />
         <Route path="/question" element={<Question />} />
+        <Route path="/card" element={<Card />} />
       </Routes>
     </BrowserRouter>
   )
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,94 +1,28 @@
-import React, { useEffect } from 'react'
+
 import './Login.css'
-import { useNavigate } from "react-router-dom";
-import liff from '@line/liff';
+import LineLoginButton from '../lineLoginButton/LineLoginButton';
+import NanoLogo from '../logo/NanoLogo';
 
 function Login() {
 
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    // Using a Promise object
-    liff
-      .init({
-        liffId: '2001717034-xGeOXRR5', // Use own liffId
-      })
-
-  }, [])
-
-  const handleLineLogin = async () => {
-    console.log('in login func')
-    await liff.init({ liffId: '2001713049-4ZwejYYb' })
-    console.log('below init line')
-    if (!liff.isLoggedIn()) {
-      console.log('ก่อน login')
-      liff.login()
-      console.log('หลัง login')
-    }
-
-    const profile = liff.getProfile()
-    console.log('login แล้ว', profile)
-  }
-
-  const handleLogout = () => {
-    liff.logout();
-    console.log('logouttttttttt')
-
-
-  }
-
   return (
+    <>
+      <div className='container text-center mt-5'>
+        <div className='row '>
+          <div className='col-12 mt-5 pt-5'>
+            
+            <NanoLogo  />
+          </div>
+        </div>
+        <div className='row mt-5 pt-5'> 
+          <div className='col-12 mt-5 pt-5'>
+            <LineLoginButton />
+          </div>
+        </div>
+        </div>
 
-    <div className="Auth-form-container">
-      <div className="Auth-form">
-        <form className="Auth-form-content">
-          <div className='card-image'>
-            <img src="src/assets/na.png" alt="" />
-          </div>
-          <br />
-          <h3 className='text-dark text-center'>Sign In</h3>
-
-          <div className="form-group mt-3">
-            <label>Email address</label>
-            <input
-              type="email"
-              className="form-control mt-1"
-              placeholder="Enter email"
-            />
-          </div>
-          <div className="form-group mt-3">
-            <label>Password </label>
-            <input
-              type="password"
-              className="form-control mt-1"
-              placeholder="Enter password"
-            />
-          </div>
-          <div className="d-grid gap-2 mt-3">
-            <button type="submit" className="btn btn-primary">
-              Sign In
-            </button>
-          </div>
-          
-          <div className="text-center mt-1">
-            <span className='text-dark'>Not registered yet?   </span>
-            <span className="link-primary" onClick={() => navigate("/register")}>
-              Sign Up
-            </span>
-          </div>
-          <p className="text-center mt-2 text-dark">
-            Forgot <a href="#">password?</a>
-          </p>
-        </form>
-        <div className="line-button ms-5 me-5">
-        <button type="submit" className="btn btn-success" onClick={handleLineLogin}>
-          Sign In With Line
-        </button>
-      </div>
-      </div>
-
-    </div>
-  )
+    </>
+  );
 }
 
-export default Login
+export default Login;
