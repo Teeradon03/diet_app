9c753363a7e27f4ff6dc3dda463128c5276dc590
teeradon <teeradon.zoo@gmail.com>
2023-12-01 09:30:08 +0700

    get value from input and console it

--- Frontend/src/components/customerKey/CustomerKey.jsx
+++ Frontend/src/components/customerKey/CustomerKey.jsx
@@ -1,6 +1,41 @@
+import { useState } from "react";
 
 export default function CustomerKey() {
+
+  const [message, setMessage] = useState('');
+
+  const handleChange = (event) => {
+    setMessage(event.target.value);
+  };
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    console.log(message)
+  };
+
   return (
-    <div>CustomerKey</div>
+    <div className="container mt-5 text-center">
+      <div className="row justify-content-center">
+        <div className="col-12">
+          <h2>INPUT THE SHIT KEY HERE!!! </h2>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-10 text-start mt-4">
+            <div className="mb-3">
+              <label htmlFor="" className="form-label">Pleas Input correctly shit key here </label>
+              <input type="text" className="form-control" name="key" id="key" aria-describedby="helpId" placeholder="nanoX12345" onChange={handleChange}/>
+            </div>
+          </div>
+        </div>
+ 
+        <div className="row justify-content-center  fixed-bottom mb-5 pb-5">
+          <div className="col-12  mb-5">
+            <button type='button' className="btn btn-secondary pe-5 ps-5" onClick={handleClick}>
+              Submit
+            </button>
+          </div>   
+        </div>
+      </div>
+    </div>
   )
 }
