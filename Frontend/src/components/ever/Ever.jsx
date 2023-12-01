91919e895e8a8510028914ccd8e52da74ad8fce3
teeradon <teeradon.zoo@gmail.com>
2023-12-01 11:16:58 +0700

    add goback button

--- Frontend/src/components/ever/Ever.jsx
+++ Frontend/src/components/ever/Ever.jsx
@@ -1,9 +1,16 @@
 import { Link } from "react-router-dom"
 import styles from './Ever.module.css'
+import { useNavigate } from "react-router-dom"
 export default function Ever() {
+	const navigation = useNavigate()
 	return (
-		<div className="container text-center pt-5">
-			<div className="row justify-content-center">
+		<div className="container text-center pt-3">
+			<div className="row">
+				<div className="col-2 ">
+					<button className="btn btn-secondary" onClick={() => navigation(-1)}>BACK</button>
+				</div>
+			</div>
+			<div className="row justify-content-center mt-5">
 				<div className="answer mt-5 ">
 					<h2>
 						รหัสลูกค้า
