3fc551d368f1a9a8951f5c1fb71e05755f8b1b5a
Anuwat <anuwat_bom@hotmail.co.th>
2024-01-06 14:27:15 +0700

    BMIcalculator

---
+++ Frontend/src/pages/Advice/Advice_obesitylevel1.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Image, Button } from 'antd'; // Import Button component from Ant Design
+import { Link } from 'react-router-dom';
+import { VscChevronLeft } from "react-icons/vsc";
+import styles from'./Advice.module.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+function Advice_obesitylevel1() {
+  return (
+    <div>
+        <div className={styles.Bmi1}>
+        <h2> คำแนะนำค่าดัชนีมวลกาย (BMI) </h2>
+        
+        <br />
+        <div className={styles.childadvice}>
+        <p>1. รับประทานอาหารในปริมาณที่เหมาะสม และหลีกเลี่ยงการบริโภคอาหารใน ปริมาณมากเกินไป</p>
+        <p>2. ออกกำลังกายอย่างสม่ำเสมอ อาจช่วย ลดน้ำหนักสร้างกล้ามเนื้อ และเพิ่มระดับ พลังงานในร่างกาย</p>
+        <p>3. ระบุเป้าหมายการลดน้ำหนักหรือควบคุมน้ำหนักเพื่อ ลดความเสี่ยงต่อโรคร้าย ต่างๆ ควรทำการลดน้ำหนักอย่าง ช้าๆ และด้วยวิธีที่ยั่งยืน</p>
+        <p>4. การปรึกษากับหมอหรือโภชนาการจะ ช่วยในการกำหนดแผนการรักษาและการดูแลสุขภาพที่เหมาะสมสำหรับคุณ</p>
+        <p>5. ความเครียดและข้อกังวลอาจมีผลต่อการควบคุมน้ำหนักและสุขภาพดังนั้นควร รักษาสุขภาพใจด้วยการปฏิบัติสิ่งที่ ทำให้คุณรู้สึกผ่อนคลาย</p>
+        </div>
+        <br />
+
+        <div >
+          <Link to="/Bmi_obesitylevel1">
+          <button className={styles.backbutton}>ย้อนกลับ</button> 
+          </Link>
+        </div>
+      </div>
+      <div className={styles.chevronicon}>
+        <Link to="/Bmi_obesitylevel1">
+        <Button // Changed button to Ant Design's Button component
+          shape="circle"
+          style={{ left: 10, top: 10, fontSize:'22px', width: '50px', height: '50px'  }}
+          icon={<VscChevronLeft />}
+        />
+      </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Advice_obesitylevel1
---
+++ Frontend/src/pages/Advice/Advice_obesitylevel3.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Image, Button } from 'antd'; // Import Button component from Ant Design
+import { Link } from 'react-router-dom';
+import { VscChevronLeft } from "react-icons/vsc";
+import styles from'./Advice.module.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+function Advice_obesitylevel3() {
+  return (
+    <div>
+        <div className={styles.Bmi1}>
+        <h2> คำแนะนำค่าดัชนีมวลกาย (BMI) </h2>
+        
+        <br />
+        <div className={styles.childadvice}>
+        <p>1. รับประทานอาหารในปริมาณที่เหมาะสม และหลีกเลี่ยงการบริโภคอาหารใน ปริมาณมากเกินไป</p>
+        <p>2. ลดการบริโภคอาหารที่มีความสูงใน โปรตีนที่ไม่ดีเช่นอาหารหมักหรืออาหาร จางๆ และหลีกเลี่ยงอาหารหรือเครื่องดื่ม ที่มีความสูงในน้ำตาลและไขมัน</p>
+        <p>3. ระบุเป้าหมายการลดน้ำหนักหรือควบคุมน้ำหนัก เพื่อลดความเสี่ยงต่อโรคร้าย ต่างๆ ควรทำการลดน้ำหนักอย่าง ช้าๆ และด้วยวิธีที่ยั่งยืน</p>
+        <p>4. เลือกกินให้มากๆ ลดอาหารหวาน ของหวาน ขนม เครื่องดื่มที่มีน้ำตาล งด หรือหลีกเลี่ยงของทอด อาหารไขมันสูง</p>
+        </div>
+        <br />
+
+        <div >
+          <Link to="/Bmi_obesitylevel3">
+          <button className={styles.backbutton}>ย้อนกลับ</button> 
+          </Link>
+        </div>
+      </div>
+      <div className={styles.chevronicon}>
+        <Link to="/Bmi_obesitylevel3">
+        <Button // Changed button to Ant Design's Button component
+          shape="circle"
+          style={{ left: 10, top: 10, fontSize:'22px', width: '50px', height: '50px'  }}
+          icon={<VscChevronLeft />}
+        />
+      </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Advice_obesitylevel3
