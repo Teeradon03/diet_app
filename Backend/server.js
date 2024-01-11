dbd2949a258511f9c7716388aa2d1455cf127c40
teeradon <teeradon.zoo@gmail.com>
2024-01-11 15:42:22 +0700

    edit api path and add cors

--- Backend/server.js
+++ Backend/server.js
@@ -15,62 +15,42 @@ const connectDB = require("./db/db");
 // route
 const { readdirSync } = require("fs");
 
+const cookieParser = require("cookie-parser");
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json({ limit: "10mb" }));
-app.use(cors());
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:5173"],
+  }),
+);
+
+// app.use(cookieParser());
 
 app.use(session({
-  secret: 'Hoiyaaaaaaaaaa',  // Replace with a strong secret key
+  secret: 'Hoiyaaa',  // Replace with a strong secret key
   resave: false,
   saveUninitialized: true
 }));
 
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Credentials', true);
-//   next();
-//   });
-// const isLoggedIn = require('./middleware/isLoggedIn')
-// app.use(isLoggedIn);
+
 
 connectDB();
 
-
 // readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)));
-
 
 const user = require('./routes/userRoute')
 const login = require('./routes/login')
-const form = require('./routes/formRoute')
+const form = require('./routes/formRoute');
 
 app.use('/api/user', user);
 app.use('/api/login', login)
 app.use('/api/form',form)
 
-
-
-app.post('/login', async(req, res) => {
-  try{
-        req.session.userId = "Test User";
-    res.send(`logged in as ${req.session.userId }`);
-  }catch(err){
-    console.log(err);
-  }
-});
-
-app.get('/profile', (req, res) => {
-  console.log('UserId Session', req.session.userId);
-  if (req.session.userId) {
-    // User is logged in, access userId from the session
-    const userId = req.session.userId;
-    res.send(`Profile page for user ${userId}`);
-  } else {
-    // User is not logged in
-    res.send('Please log in');
-  }
-});
+const { isLoggedIn } = require('./middleware/isLoggedIn');
+// app.use(isLoggedIn)
 
 
 
