24a35653aebf6210236f251b3abe7366c56d1ec8
teeradon <teeradon.zoo@gmail.com>
2024-01-15 16:49:50 +0700

    edit syntax

--- Frontend/src/components/Name/Name.jsx
+++ Frontend/src/components/Name/Name.jsx
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Name() {
   const [formData, setFormData] = useState({
-    firstname: '',
+    firstName: '',
     lastName: '',
     age: '',
     gender: '',
