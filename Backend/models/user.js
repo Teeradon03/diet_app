135a2bf52ca8fd2c9a8b8d2baee95caeb7f4b131
teeradon <teeradon.zoo@gmail.com>
2024-01-15 16:30:44 +0700

    update user model

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose')
 const userSchema = mongoose.Schema({
     firstName: String,
     lastName: String,
+    age: Number,
+    gender: String,
     dataOfBirth : Date,
     phone: Number,
     height: Number,
