160a55f17527a365d3aacff3fa0bae07b3615df7
teeradon <teeradon.zoo@gmail.com>
2024-01-16 09:18:19 +0700

    chagne mongo URI

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async (req, res) => {
   try {
-    await mongoose.connect(process.env.MONGO_ATLAST);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.log(error);
