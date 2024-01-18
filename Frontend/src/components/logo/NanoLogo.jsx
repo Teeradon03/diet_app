03dbb8a6e2a069c0e457f8054fcc32244aadfd5a
teeradon <teeradon.zoo@gmail.com>
2024-01-18 10:39:13 +0700

    don't want this

--- Frontend/src/components/logo/NanoLogo.jsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function NanoLogo() {
-  return (
-    <div className='logo' style={{backgroundImage : "CI-nanoverse.jpg"}}>
-        <img src="nano-logo.png" className="img-thumbnail" alt="nano-logo" ></img>
-    </div>
-  )
-}
